254d9e828a68c5b8d515b92385a16715dd34dbca
DavidCurelop <dscurelop1@eafit.edu.co>
2026-03-11 16:17:09 -0500

    fix(accounts): centralize base template and harden user validation

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.translation import gettext as _
 
 User = get_user_model()
 
@@ -38,7 +39,7 @@ class UserRegistrationTest(TestCase):
         self.assertFormError(
             response.context['form'],
             'email',
-            'A user with this email already exists.',
+            _('A user with this email already exists.'),
         )
 
     def test_registration_password_mismatch(self):
