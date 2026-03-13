d28d77528041fe8bf9866afb97950d426d60d1da
DavidCurelop <dscurelop1@eafit.edu.co>
2026-03-12 19:10:10 -0500

    feat(accounts): register password reset URLs.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,16 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
+    path(
+        'password-reset/',
+        views.UserPasswordResetView.as_view(),
+        name='password-reset',
+    ),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        views.UserPasswordResetConfirmView.as_view(),
+        name='password-reset-confirm',
+    ),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
 ]
